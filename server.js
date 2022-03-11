f39bea3d8159b23f5065e565ea15cf950555ae7f
woffordlm <lukewofford@gmail.com>
2022-03-11 10:34:25 -0500

    changed port number

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
  sequelize.sync({ force: false}).then(() => {
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
 })

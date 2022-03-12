12f4c77cc0d4232806b649ed1a4063ed815caf7e
woffordlm <lukewofford@gmail.com>
2022-03-12 10:38:13 -0500

    connect jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize("tech_blog", "root", "password", {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else { sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+  }
 
 module.exports = sequelize;
